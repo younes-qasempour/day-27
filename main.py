9f645a3620787ffa98f4035207010fcd8175ce8e
younes_qasempour <qasempouryounes@gmail.com>
2025-02-20 19:00:02 +0330

    final

--- main.py
+++ main.py
@@ -10,22 +10,30 @@ def button_clicked():
 window = Tk()
 window.title("My First GUI Program")
 window.minsize(500, 300)
+window.config(padx=20, pady=20)
 
 #Label
 my_label = Label(text="I Am a Label", font=("Arial", 16, "bold"))
-my_label.config(text="New Text")
+my_label.config(text="New Text", padx=10, pady=10)
 my_label.grid(column=0, row=0)
 
 
 #Button
 button = Button(text="Click Me", command=button_clicked)
+button.config(padx=10, pady=10)
 button.grid(column=1, row=1)
+
+
+#Button
+button_2 = Button(text="Click Me", command=button_clicked)
+button_2.config(padx=10, pady=10)
+button_2.grid(column=2, row=0)
 
 
 #Entery
 input = Entry(width=15)
 print(input.get())
-input.grid(column=2, row=2)
+input.grid(column=3, row=2)
 
 
 
---
+++ mile-to-km-converter.py
@@ -0,0 +1,50 @@
+from tkinter import *
+
+def convertor():
+    x = round(float(entry.get()) * 1.609, 2)
+    km_value_label.config(text=x)
+
+
+window = Tk()
+window.title("Mile to Km Converter")
+window.minsize(300, 100)
+
+#Label
+miles_label = Label(text="Miles")
+miles_label.grid(column=2, row=0)
+
+#Label
+km_label = Label(text="Km")
+km_label.grid(column=2, row=1)
+
+#Label
+is_equal_label = Label(text="is equal to")
+is_equal_label.grid(column=0, row=1)
+
+#Label
+km_value_label = Label(text="0")
+km_value_label.grid(column=1, row=1)
+
+#Entry
+entry = Entry()
+entry.grid(column=1, row=0)
+
+#Button
+button = Button(text="Click Me", command=convertor)
+button.grid(column=1, row=2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
